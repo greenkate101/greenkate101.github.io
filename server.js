ea992522539958d26af3eb705d8411be2c9a4d8a
Kate Green <greenkate@me.com>
2016-11-09 12:22:51 -0500

    added

--- server.js
+++ server.js
@@ -14,25 +14,3 @@ app.use(express.static(__dirname + '/src/css'));
 
 app.listen(port);
 
-var database = {
-	sharksId: 1,
-	sharks: []
-};
-
-app.get('/api/sharks', function (req, res) {
-	res.join(database.sharks);
-});
-
-app.post('/api/sharks', function(req, res) {
-	var body = req.body;
-	var name = body.name;
-	var weight = body.weight;
-	var location = body.location;
-
-	var newShark = {
-		id: database.sharks.push(newShark);
-	};
-		database.sharkId++;
-
-		res.json(newShark);
-}
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,6 +1,20 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var ReactRouter = require('react-router');
+
+//var {Router, Route, hashHistory} = require('react-router'); other way to do lines 7-9. but not recommended
+
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var IndexRoute = ReactRouter.IndexRoute;
+var hashHistory = ReactRouter.hashHistory;
 
 var App = require('./components/App.jsx');
 
-ReactDOM.render(<App />, document.querySelector('#app'));
+var jsx = (
+	<Router history={hashHistory}>
+		<Route path="/" component={App}></Route>
+	</Router>
+);
+
+ReactDOM.render(jsx, document.querySelector('#app'));
