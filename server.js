d0a75e911901cccb2497195c2877b85741064897
Kate Green <greenkate@me.com>
2016-11-10 11:51:32 -0500

    added db

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
-var express = require('express');
+var lowdb = require('lowdb');
+var fileAsync = require('lowdb/lib/file-async');
+var shortid = require('shortid');
 var bodyParser = require('body-parser');
+var express = require('express');
+
+var db = lowdb('db.json', {storage: fileAsync});
+
 var app = express();
 var port = 3000;
 
 app.use(bodyParser());
-
 app.use(express.static(__dirname + '/public'));
 app.use(express.static(__dirname + '/dist'));
 app.use(express.static(__dirname + '/lib'));
 //remove this when we start buliding our sass
 app.use(express.static(__dirname + '/src/css'));
 //dirname
+
+db.defaults({
+
+}).value();
 
 app.listen(port);
 
